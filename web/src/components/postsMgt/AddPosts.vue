b595092c191ff6aee2bef4d85227dae98364cab7
Nethercat7 <nethercat7@foxmail.com>
2020-12-24 18:20:44 +0800

    富文本编辑器汉化

--- web/src/components/postsMgt/AddPosts.vue
+++ web/src/components/postsMgt/AddPosts.vue
@@ -108,19 +108,20 @@
             toolbar: {
               //配置工具栏
               container: [
-                ["bold", "italic", "underline", "strike"],
-                ["blockquote", "code-block"],
-                [{header: 1}, {header: 2}],
-                [{list: "ordered"}, {list: "bullet"}],
-                [{script: "sub"}, {script: "super"}],
-                [{indent: "-1"}, {indent: "+1"}],
-                [{direction: "rtl"}],
-                [{size: ["small", false, "large", "huge"]}],
-                [{header: [1, 2, 3, 4, 5, 6, false]}],
-                [{font: []}],
-                [{color: []}, {background: []}],
-                [{align: []}],
-                ["link", "image", "video"],
+                ["bold", "italic", "underline", "strike"], // 加粗 斜体 下划线 删除线
+                ["blockquote", "code-block"], // 引用  代码块
+                [{ header: 1 }, { header: 2 }], // 1、2 级标题
+                [{ list: "ordered" }, { list: "bullet" }], // 有序、无序列表
+                [{ script: "sub" }, { script: "super" }], // 上标/下标
+                [{ indent: "-1" }, { indent: "+1" }], // 缩进
+                // [{'direction': 'rtl'}],                         // 文本方向
+                [{ size: ["small", false, "large", "huge"] }], // 字体大小
+                [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
+                [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
+                [{ font: [] }], // 字体种类
+                [{ align: [] }], // 对齐方式
+                ["clean"], // 清除文本格式
+                ["link", "image", "video"] // 链接、图片、视频
               ],
               handlers: {
                 image: function (val) {
