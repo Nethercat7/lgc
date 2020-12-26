ebbb06d9be3d8979d913b265478ef49981ad9e4b
Nethercat7 <nethercat7@foxmail.com>
2020-12-26 22:29:13 +0800

    更新菜单选项

--- web/src/components/Index.vue
+++ web/src/components/Index.vue
@@ -15,28 +15,41 @@
 
     <el-row type="flex">
       <el-col class="content" :span="3">
-        <el-menu :default-openeds="['1','2','3','4']" background-color="#23282D" text-color="#ffffff">
+        <el-menu :default-openeds="['1','2','3','4','5']" background-color="#23282D" text-color="#ffffff">
           <el-submenu index="1">
             <template slot="title"><i class="el-icon-user-solid"></i>用户管理</template>
             <el-menu-item @click="goto('/userMgt/users')">所有用户</el-menu-item>
             <el-menu-item @click="goto('/userMgt/add')">注册用户</el-menu-item>
+            <el-menu-item>修改用户状态</el-menu-item>
           </el-submenu>
           <el-submenu index="2">
             <template slot="title"><i class="el-icon-s-custom"></i>角色管理</template>
             <el-menu-item @click="goto('/roleMgt/roles')">所有角色</el-menu-item>
             <el-menu-item @click="goto('/roleMgt/add')">添加角色</el-menu-item>
+            <el-menu-item>修改角色状态</el-menu-item>
           </el-submenu>
           <el-submenu index="3">
             <template slot="title"><i class="el-icon-notebook-1"></i>分类管理</template>
             <el-menu-item @click="goto('/garbage/category')">分类类别</el-menu-item>
             <el-menu-item @click="goto('/garbage/table')">垃圾分类</el-menu-item>
           </el-submenu>
+
           <el-submenu index="4">
             <template slot="title"><i class="el-icon-edit"></i>文章管理</template>
             <el-menu-item @click="goto('/postsMgt/table')">所有文章</el-menu-item>
             <el-menu-item @click="goto('/postsMgt/add')">发布文章</el-menu-item>
             <el-menu-item @click="goto('/postsMgt/categories')">文章类别</el-menu-item>
           </el-submenu>
+
+            <el-submenu index="5">
+              <template slot="title"><i class="el-icon-s-tools"></i>系统管理</template>
+              <el-menu-item>小程序设置</el-menu-item>
+              <el-menu-item>后台系统设置</el-menu-item>
+              <el-menu-item>单点登录设置</el-menu-item>
+              <el-menu-item>查看反馈意见</el-menu-item>
+              <el-menu-item>查看操作日志</el-menu-item>
+              <el-menu-item>查看登录日志</el-menu-item>
+            </el-submenu>
         </el-menu>
       </el-col>
 
